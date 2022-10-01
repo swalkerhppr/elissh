45fc9dc36913cd8e494f5230ff9907715dbb929b
swalkerhppr <Stephen.d.a.walker@gmail.com>
2022-09-30 22:14:49 -0500

    Update repo path

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Elissh.Mixfile do
     [
       {:sshex, "~> 2.2"},
       {:yaml_elixir, "~> 2.0"},
-      {:io_tty, git: "https://github.com/swalker90/elixir-io_tty.git", tag: "v1.0.1"}
+      {:io_tty, git: "https://github.com/swalkerhppr/elixir-io_tty.git", tag: "v1.0.1"}
     ]
   end
 end
